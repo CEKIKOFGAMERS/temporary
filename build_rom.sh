b1f882e259982914bcc57daf495b0935c7354242
Kristoforus Ananta Putra Magallan <maga64613@gmail.com>
2023-07-30 17:17:48 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest -b arrow-13.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/itsmeNaraa/local_manifest.git --depth 1 -b ArrowOS .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/itsmeNaraa/local_manifest.git --depth 1 -b nad-12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch arrow_stone-userdebug
+lunch nad_stone-userdebug
 export BUILD_USERNAME=Kristo
 export BUILD_HOSTNAME=Kristo
 export KBUILD_USERNAME=Kristo
